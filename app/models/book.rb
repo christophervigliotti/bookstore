8b0090b3361717ca20714ef5fc74e2c9a2f216ff
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-30 18:26:49 +0000

    class 5 lab 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,4 +9,7 @@ class Book < ActiveRecord::Base
   validates :price,
             numericality: { greater_than_or_equal_to: 0 },
             if: 'price.present?'
+  def average_stars
+    reviews.average(:stars)
+  end
 end
