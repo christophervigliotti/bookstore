35288d464df4a09a37988d8dd2ff16bfb8c1e264
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-30 19:51:06 +0000

    stopping here

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'autoprefixer-rails'
 gem 'kaminari'
 gem 'kaminari-bootstrap', '~> 3.0.1'
 
+# added for class 5
+gem 'powerpack'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ActiveRecord::Base
+
   has_many :reviews, dependent: :destroy
+
   scope :bargains, -> { where('price < 10.00') }
   scope :by, ->(author) { where('author = ?', author) }
+
   validates :title, :author, :pages, :price, presence: true
   validates :pages,
             numericality: { only_integer: true, greater_than_or_equal_to: 0 },
@@ -9,7 +12,13 @@ class Book < ActiveRecord::Base
   validates :price,
             numericality: { greater_than_or_equal_to: 0 },
             if: 'price.present?'
+
   def average_stars
-    reviews.average(:stars)
+    if reviews.loaded?
+      reviews.map(&:stars).compact.average
+    else
+      reviews.average(:stars)
+    end
   end
+
 end
